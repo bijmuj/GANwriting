246d7e87152c98f0c6af999d619dc51190fad8ae
Kartikeya Shorya <kartikeyashorya@gmail.com>
2021-09-18 22:12:38 +0530

    Added convert_pic_to_mini_arrays function

--- utils.py
+++ utils.py
@@ -455,3 +455,35 @@ def cleanup_temp_files(id):
             os.rmdir(dirname)
         except:
             print(pdf, "failed to delete")
+
+
+def convert_pic_to_mini_array(path):
+    """
+    Crop out images of individual handwrittten words from the whole image.
+
+    Args:
+        path (string): Path to the image file having handwritten text.
+
+    Returns:
+        cropped_images (List[np.array]): A list of np.array of imgs of words cropped out from the source image.
+    """
+
+    try:
+        img = cv2.imread(path)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 25))
+        dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+        contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                                        cv2.CHAIN_APPROX_NONE)
+        im2 = img.copy()
+        cropped_images = []
+        for cnt in contours:
+            x, y, w, h = cv2.boundingRect(cnt)
+            rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cropped = im2[y:y + h, x:x + w]
+            cropped_images.append(np.array(cropped))
+
+        return cropped_images
+    except:
+        print('path cannot be null')
